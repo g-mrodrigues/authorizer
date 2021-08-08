4ed6e73f9ce0bc8b7b610b94ae3be90dd525d772
Gustavo Rodrigues <gustavo.rodrigues@picpay.com>
2021-08-08 19:17:38 -0300

    Adding structure to the project

---
+++ src/Adapters/Controllers/AuthorizeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Adapters\Controllers;
+
+class AuthorizeController
+{
+    public function __construct()
+    {
+
+    }
+
+    public function authorize(string $input): string
+    {
+
+    }
+}
---
+++ src/Adapters/Repositories/AccountRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Authorize\Adapters\Repositories;
+
+interface AccountRepositoryInterface
+{
+
+}
